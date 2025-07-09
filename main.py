731e9c8f0f0a9cbb1fd75d5fc56284c0261c14d7
hj-08 <hjs07920792@gmail.com>
2025-07-09 14:29:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title('웨에에에에에에에에에에엡')
 name = st.text_input('이름을 입력해주세요 : ')
 menu = st.selectbox('가장 많이 쓰는 앱은? :', ['유튜브','인스타', '카톡'])
-time = st.slider('하루 사용 시간은?', 0, 10000000000000000, 3)
+time = st.slider('하루 사용 시간은?', 0, 100000, 3)
 if st.button('나의 디지털 습관') : 
   st.write(f'{name}님! {menu}를 {time}시간 사용 중이시군요. 균형 잡힌 사용이 중요해요!')
